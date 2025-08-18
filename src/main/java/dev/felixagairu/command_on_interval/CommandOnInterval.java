34b7ded30c3fa4a3d1b64ff925be1893f2d94ce5
Felix Agairu <FelixAgairu@outlook.com>
2025-08-18 19:21:54 +0800

    Relicensed to MPL v2
    Relicensed project from MIT to MPL 2.0 on 2025-08-18.

--- src/main/java/dev/felixagairu/command_on_interval/CommandOnInterval.java
+++ src/main/java/dev/felixagairu/command_on_interval/CommandOnInterval.java
@@ -1,3 +1,9 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
 package dev.felixagairu.command_on_interval;
 
 import dev.felixagairu.command_on_interval.tools.DataLoader;
@@ -9,33 +15,33 @@ import org.slf4j.LoggerFactory;
 import java.util.ArrayList;
 
 public class CommandOnInterval implements ModInitializer {
-	public static final String MOD_ID = "command-on-interval";
-	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+    public static final String MOD_ID = "command-on-interval";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
-	@Override
-	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-		commandOnIntervalStart();
-	}
+    @Override
+    public void onInitialize() {
+        // This code runs as soon as Minecraft is in a mod-load-ready state.
+        // However, some things (like resources) may still be uninitialized.
+        // Proceed with mild caution.
+        commandOnIntervalStart();
+    }
 
-	private void commandOnIntervalStart() {
-		try {
-			if(DataLoader.load() && !DataLoader.listOfCommands.isEmpty()) {
-				ArrayList<IntervalRegister> instances = new ArrayList<>();
-				for (int i = 0; i < DataLoader.listOfCommands.size(); i++) {
-					instances.add(new IntervalRegister(
-							DataLoader.listOfUnitUseTicks.get(i),
-							DataLoader.listOfDelay.get(i),
-							DataLoader.listOfInterval.get(i),
-							DataLoader.listOfCommands.get(i)
-					));
-				}
-			}
-		} catch (Exception e) {
-			System.err.println("[command-on-interval] Failed to initialize configs: " + e.getMessage());
-			DataLoader.reset();
-		}
-	}
-}
+    private void commandOnIntervalStart() {
+        try {
+            if (DataLoader.load() && !DataLoader.listOfCommands.isEmpty()) {
+                ArrayList<IntervalRegister> instances = new ArrayList<>();
+                for (int i = 0; i < DataLoader.listOfCommands.size(); i++) {
+                    instances.add(new IntervalRegister(
+                            DataLoader.listOfUnitUseTicks.get(i),
+                            DataLoader.listOfDelay.get(i),
+                            DataLoader.listOfInterval.get(i),
+                            DataLoader.listOfCommands.get(i)
+                    ));
+                }
+            }
+        } catch (Exception e) {
+            System.err.println("[command-on-interval] Failed to initialize configs: " + e.getMessage());
+            DataLoader.reset();
+        }
+    }
+}/**/
--- src/main/java/dev/felixagairu/command_on_interval/tools/IntervalRegister.java
+++ src/main/java/dev/felixagairu/command_on_interval/tools/IntervalRegister.java
@@ -1,3 +1,9 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
 package dev.felixagairu.command_on_interval.tools;
 
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
